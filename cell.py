92fc06d82004ac92068a538bd74beed82d7058dc
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2022-10-13 15:14:55 +0200

    Random mines

--- cell.py
+++ cell.py
@@ -2,15 +2,13 @@ import pygame
 
 from constants import CELL_SIZE
 
-
 class Cell(pygame.sprite.Sprite):
-
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, color='black'):
         super().__init__()
         self.screen = screen
         self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
         pygame.draw.rect(self.image, 'white', pygame.Rect(0, 0, CELL_SIZE, CELL_SIZE))
-        pygame.draw.rect(self.image, 'black', pygame.Rect(1, 1, CELL_SIZE - 2, CELL_SIZE - 2))
+        pygame.draw.rect(self.image, color, pygame.Rect(1, 1, CELL_SIZE - 2, CELL_SIZE - 2))
         self.rect = self.image.get_rect(topleft=(x * CELL_SIZE, y * CELL_SIZE))
         self.returned = False
         self.flag = False
@@ -22,4 +20,4 @@ class NumberedCell(Cell):
 
 class MinedCell(Cell):
     def __init__(self, screen, x, y):
-        super().__init__(screen, x, y)
+        super().__init__(screen, x, y, color='red')
--- game.py
+++ game.py
@@ -1,6 +1,9 @@
+from lib2to3.pgen2.token import NUMBER
 import pygame
+import random
 from cell import Cell
-from constants import NUMBER_OF_SIDE_CELLS
+from constants import NUMBER_OF_SIDE_CELLS, NUMBER_OF_MINES
+from cell import MinedCell
 
 class Game:
   def __init__(self, screen) -> None:
@@ -8,8 +11,19 @@ class Game:
 
   def run(self):
     cells = []
+    mines_coords = []
+    for i in range (NUMBER_OF_MINES):
+      while True:
+        mine_coords = (random.randint(0, NUMBER_OF_SIDE_CELLS), random.randint(0, NUMBER_OF_SIDE_CELLS))
+        if not mines_coords.__contains__(mine_coords): break
+      mines_coords.append(mine_coords) 
+
+
     for i in range(NUMBER_OF_SIDE_CELLS):
       for j in range(NUMBER_OF_SIDE_CELLS):
-        cells.append(Cell(self.screen, i, j))
+        if mines_coords.__contains__((i, j)):
+          cells.append(MinedCell(self.screen, i, j))
+        else: 
+          cells.append(Cell(self.screen, i, j))
     cells = pygame.sprite.Group(cells).draw(self.screen)
       
