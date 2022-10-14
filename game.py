99948cf3d75bf8cff328287914431ebba7f7f96a
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2022-10-14 08:47:10 +0200

    Random mines in constructor

--- game.py
+++ game.py
@@ -13,17 +13,22 @@ class Game:
     cells = []
     mines_coords = []
     for i in range (NUMBER_OF_MINES):
+      # Same as `do (new mine coords) while (same coords)`
       while True:
         mine_coords = (random.randint(0, NUMBER_OF_SIDE_CELLS), random.randint(0, NUMBER_OF_SIDE_CELLS))
-        if not mines_coords.__contains__(mine_coords): break
-      mines_coords.append(mine_coords) 
+        if not self.mines_coords.__contains__(mine_coords): break
+      self.mines_coords.append(mine_coords) 
+
+    print(self.mines_coords)
 
 
     for i in range(NUMBER_OF_SIDE_CELLS):
       for j in range(NUMBER_OF_SIDE_CELLS):
-        if mines_coords.__contains__((i, j)):
-          cells.append(MinedCell(self.screen, i, j))
+        if self.mines_coords.__contains__((i, j)):
+          self.cells.append(MinedCell(self.screen, i, j))
         else: 
-          cells.append(Cell(self.screen, i, j))
-    cells = pygame.sprite.Group(cells).draw(self.screen)
+          self.cells.append(Cell(self.screen, i, j))
+
+  def run(self):
+    pygame.sprite.Group(self.cells).draw(self.screen)
       
