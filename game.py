cbab023dbb265f6578c8c8b07d00f28ee4a3aee1
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2022-10-14 10:18:50 +0200

    Numbered cells

--- game.py
+++ game.py
@@ -1,29 +1,42 @@
-from lib2to3.pgen2.token import NUMBER
 import pygame
 import random
-from cell import Cell
 from constants import NUMBER_OF_SIDE_CELLS, NUMBER_OF_MINES
-from cell import MinedCell
+from cell import Cell, MinedCell, NumberedCell
+
 
 class Game:
-  def __init__(self, screen) -> None:
-    self.screen = screen
-    self.cells = []
-    self.mines_coords = []
-    for i in range (NUMBER_OF_MINES):
-      # Same as `do (new mine coords) while `
-      while True:
-        mine_coords = (random.randint(0, NUMBER_OF_SIDE_CELLS - 1), random.randint(0, NUMBER_OF_SIDE_CELLS - 1))
-        if not self.mines_coords.__contains__(mine_coords): break
-      self.mines_coords.append(mine_coords)
+    def __init__(self, screen):
+        self.screen = screen
+        self.cells = []
+        self.mines_coords = []
+        for i in range(NUMBER_OF_MINES):
+            # Same as `do (new mine coords) while `
+            while True:
+                mine_coords = (random.randint(0, NUMBER_OF_SIDE_CELLS - 1),
+                               random.randint(0, NUMBER_OF_SIDE_CELLS - 1))
+                if not self.mines_coords.__contains__(mine_coords):
+                    break
+            self.mines_coords.append(mine_coords)
 
-    for i in range(NUMBER_OF_SIDE_CELLS):
-      for j in range(NUMBER_OF_SIDE_CELLS):
-        if self.mines_coords.__contains__((i, j)):
-          self.cells.append(MinedCell(self.screen, i, j))
-        else: 
-          self.cells.append(Cell(self.screen, i, j))
+        # Draw cells and mines
+        for i in range(NUMBER_OF_SIDE_CELLS):
+            for j in range(NUMBER_OF_SIDE_CELLS):
+                if self.mines_coords.__contains__((i, j)):
+                    self.cells.append(MinedCell(self.screen, i, j))
+                else:
+                  # Add numbered cells
+                  number_of_mines = 0
+                  # Cells around the actual cell
+                  range_cells = [(i - 1, j), (i, j - 1), (i - 1, j-1),
+                                (i + 1, j), (i, j + 1), (i + 1, j + 1), (i - 1, j + 1), (i + 1, j - 1)]
+                  for cell_coord in range_cells:
+                    if self.mines_coords.__contains__(cell_coord):
+                      number_of_mines += 1
+                  if number_of_mines > 0:
+                    self.cells.append(NumberedCell(self.screen, i, j, number=number_of_mines))
+                  else: 
+                    self.cells.append(Cell(self.screen, i, j))
+       
 
-  def run(self):
-    pygame.sprite.Group(self.cells).draw(self.screen)
-      
+    def run(self) -> None:
+        pygame.sprite.Group(self.cells).draw(self.screen)
