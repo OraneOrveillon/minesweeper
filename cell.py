cc648c030b983ec2f9bc385e6d3bcccd893554ee
OraneOrveillon <71334686+OraneOrveillon@users.noreply.github.com>
2022-10-17 09:40:54 +0200

    Bigger cells + new assets

--- cell.py
+++ cell.py
@@ -6,9 +6,8 @@ class Cell(pygame.sprite.Sprite):
     def __init__(self, screen, x, y, color='black'):
         super().__init__()
         self.screen = screen
-        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
-        pygame.draw.rect(self.image, 'white', pygame.Rect(0, 0, CELL_SIZE, CELL_SIZE))
-        pygame.draw.rect(self.image, color, pygame.Rect(1, 1, CELL_SIZE - 2, CELL_SIZE - 2))
+        self.number = number
+        self.image = image or pygame.image.load('minesweeper/assets/'+str(number)+'.png')
         self.rect = self.image.get_rect(topleft=(x * CELL_SIZE, y * CELL_SIZE))
         self.returned = False
         self.flag = False
